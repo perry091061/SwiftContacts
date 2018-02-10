cb2d4428b077f24a643e2456512d88f9f0332201
perry <perry091061@gmail.com>
2018-02-10 15:52:33 +0000

    Contacts test for data

--- SwiftContactsTests/SwiftContactsTests.swift
+++ SwiftContactsTests/SwiftContactsTests.swift
@@ -5,25 +5,36 @@
 //  Created by Perry Davies on 09/02/2018.
 //  Copyright © 2018 Perry Davies. All rights reserved.
 //
-
+import Contacts
 import XCTest
 @testable import SwiftContacts
 
 class SwiftContactsTests: XCTestCase {
+    var contacts : [CNContact] = []
+    var dataModel : DataModel?
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        dataModel = DataModel()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Put teardown code here. This method is called after the invocation of each test methoddata in the class.
         super.tearDown()
+        dataModel = nil
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        if let count = dataModel?.contacts?.count
+        {
+            XCTAssert(count > 0)
+            print("Contacts cotains \(count) entries")
+        }
+        else
+        {
+            XCTFail("No Contacts available")
+        }
+        
     }
     
     func testPerformanceExample() {
